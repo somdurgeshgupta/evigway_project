65c31b7d30522998b00a7ccd6614882ab02798de
somdurgeshgupta <somdurgeshgupta@gmail.com>
2025-01-02 16:31:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  createForm: FormGroup;
   title = 'evigway-application';
+  users: any;
+
+  fields = {
+    title: 'Simple Form',
+    fields: [
+      {
+        type: 'text',
+        label: 'Name',
+        name: 'name',
+        validations: ['required'],
+      },
+      {
+        type: 'email',
+        label: 'Email',
+        name: 'email',
+        validations: ['required'],
+      }
+    ],
+  };
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.createForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  onSubmit() {
+    this.http
+      .post('http://localhost:4000/savedata', this.createForm.value)
+      .subscribe((res: any) => {
+        this.getData();
+      });
+  }
+
+  getData() {
+    this.http.get('http://localhost:4000/data').subscribe((res: any) => {
+      this.users = res.data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
